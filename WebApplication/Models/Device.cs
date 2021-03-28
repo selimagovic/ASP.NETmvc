88f09eb282e220400defbda29286f71e23f84964
Selim Agovic <selimagovic@gmail.com>
2021-03-28 09:00:48 +0200

    Initial Commit

---
+++ WebApplication/Models/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Device
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Device Name")]
+        [Required]
+        public string DeviceName { get; set; }
+        public string DeviceType { get; set; }
+        public int DeviceTypeId { get; set; }
+    }
+}
